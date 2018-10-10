fc89dc095ead63f552af36c5e0ad290702cf3d63
Tino Rusch <tino.rusch@gmail.com>
2018-10-10 10:13:11 +0200

    add error slice implementation + github.com/pkg/errors wrappers.

---
+++ errors.go
@@ -0,0 +1,29 @@
+package errors
+
+import (
+	"github.com/pkg/errors"
+)
+
+/**
+ * The following are just wrappers around github.com/pkg/errors to prevent import headache
+ */
+
+// Wrap annotates the error with a message
+func Wrap(err error, message string) error {
+	return errors.Wrap(err, message)
+}
+
+// Wrapf annotates the errors with a formatted message
+func Wrapf(err error, format string, args ...interface{}) error {
+	return errors.Wrapf(err, format, args...)
+}
+
+// WithStack annotates the error with a stack trace
+func WithStack(err error) error {
+	return errors.WithStack(err)
+}
+
+// New creates a new single error with a given message
+func New(msg string) error {
+	return errors.New(msg)
+}
